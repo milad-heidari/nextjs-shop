a6240d5ebda0794010d3f6a39fdfc40628f5af4a
milad heidari <milad.heidari.dev@gmail.com>
2022-01-25 11:04:27 +0330

    add head title and meta, import ProductList component, get products in getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
-export default function Home() {
+import Head from 'next/head';
+import ProductsList from '../components/products/productsList/ProductsList';
+import * as http from '../services/Http';
+export default function Home({products}) {
   return (
+    <>
+    <Head>
+      <title>next js shop</title>
+      <meta name='description' content='online shop develop with next js' />
+    </Head>
     <div className={styles.container}>
-      shop
+      <ProductsList products={products}/>
     </div>
+    </>
   )
+}
+
+export async function getStaticProps(){
+  const products = await http.get('/products')
+  return {
+    props:{products:products.data}
+  }
 }
